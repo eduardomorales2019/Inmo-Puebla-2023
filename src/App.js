aa0a8ec3d98e503c1694f18057e83d081fda9bca
EduardoMorales <eduardo@inmo-puebla.com>
2023-01-17 22:44:12 -0600

    se crea repo de InmoPuebla

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='hero'>
+          <h2 className='sitio'>Sitio en construcción</h2>
+          <h1> Inmo - PueblAA</h1>
+          {/* <h3 className="naming">
+            compra, renta , administracion de propiedades
+          </h3> */}
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
