e9b16de48783c0803da44a2fff4cda03a175d014
EduardoMorales <eduardo@inmo-puebla.com>
2023-01-18 00:12:19 -0600

    Ãcomit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css"
+import ReactGA from "react-ga"
 
+console.log(ReactGA)
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <div className='hero'>
           <h2 className='sitio'>Sitio en construcción</h2>
-          <h1> Inmo - PueblAA</h1>
-          {/* <h3 className="naming">
+          <h1> Inmo - Puebla</h1>
+          <h3 className='naming'>
             compra, renta , administracion de propiedades
-          </h3> */}
+          </h3>
         </div>
       </header>
     </div>
